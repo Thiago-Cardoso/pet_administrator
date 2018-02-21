c75e6342d3d3ea2c035b614a8c897170bedaabf2
Thiago-Cardoso <trovador182@gmail.com>
2018-02-21 16:07:30 -0300

    changed models and create new relationship

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,6 +1,17 @@
 class Sell < ApplicationRecord
   include Fae::BaseModelConcern
+
+  validates :client, presence: true
   enum status: { finished: 0, canceled: 1 }
+
+  belongs_to :discount
+  belongs_to :client
+
+  has_many :sell_products
+  has_many :products, through: :sell_products
+  
+  has_many :sell_services
+  has_many :services, through: :sell_services
 
   def fae_display_field
     
@@ -10,6 +21,4 @@ class Sell < ApplicationRecord
     order(:id)
   end
 
-  belongs_to :discount
-  belongs_to :client
 end
