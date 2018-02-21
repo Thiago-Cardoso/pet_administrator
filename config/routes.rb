7a70be1a6d428e6dbb1d95cbc3edd277b6bce163
Thiago-Cardoso <trovador182@gmail.com>
2018-02-21 11:09:26 -0300

    resources and create scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 require 'sidekiq/web'
+
 Rails.application.routes.draw do
+
+  namespace :admin do
+    resources :services
+    resources :campaigns
+    resources :products
+    resources :suppliers
+    resources :sells
+    resources :discounts
+    resources :clients
+  end
+  # mount Fae below your admin namespec
+  mount Fae::Engine => '/admin'
+
   mount Sidekiq::Web => '/sidekiq'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
